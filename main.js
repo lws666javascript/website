b592212780b88fa14c2f21c2816561044c3ff68e
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-08 15:03:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-//
+//获取元素
+let userInput = document.querySelector("#UserInput"),
+    userSubmit = document.querySelector("#UserSubmit"),
+    mainPage = document.querySelector("#mainPage");
+userSubmit.addEventListener("click",function(){
+  let v = userInput.value;
+  let iframe = document.creatElement("iframe");
+  iframe.src = v;
+  iframe.class = "adding-iframe";
+  mainPage.appendChild(iframe);
+  createCloseBtn(iframe);
+})
+function createCloseBtn(e){
+  let c = document.createElement("div");
+  c.innerHTML = "关闭";
+  c.class = "closePageButton";
+  c.addEventListener("click",function(){
+    e.remove();
+    c.remove();
+  });
+}
