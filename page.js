d4f70a514cd1e4016879fd4125753d3a08fe1de7
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-09 10:08:43 +0800

    Update page.js

--- page.js
+++ page.js
@@ -1,11 +1,15 @@
 class page{
-  constructor({url,dir}){
+  constructor({url,dir,size}){
     this.url = url;
     this.dir = dir;
+    this.size = size;
     this.tools = null;
   }
   show(){
     
+  }
+  change(){
+  
   }
   render(){
     
