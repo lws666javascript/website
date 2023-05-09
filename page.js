ee35ef1f8afcb070722f90ec1c0bd9c5a7f7597b
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-09 10:06:47 +0800

    Create page.js

---
+++ page.js
@@ -0,0 +1,22 @@
+class page{
+  constructor({url,dir}){
+    this.url = url;
+    this.dir = dir;
+    this.tools = null;
+  }
+  show(){
+    
+  }
+  render(){
+    
+  }
+  move(){
+  
+  }
+  bind(){
+  
+  }
+  createTools(){
+  
+  }
+}
