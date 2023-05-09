7ec53a5af4b39572487e98c93f64b5cc4dca757c
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-09 10:25:01 +0800

    Update page.js

--- page.js
+++ page.js
@@ -1,17 +1,19 @@
 class page{
   constructor({url,dir,size}){
     this.url = url;
-    this.dir = dir;
-    this.size = size;
+    this.dir = dir || [];
+    this.size = size || [];
     this.tools = null;
   }
-  show(){
-    
+  show(p){
+    let i = document.createElement("iframe");
+    i.url = this.url;
+    i.className = ""
   }
-  change(){
-  
-  }
-  render(){
+  render({url,dir,size}){
+    this.url = url;
+    this.dir = dir;
+    this.size = size;
     
   }
   move(){
