c61ea4bfa6a77712e795298b121741c20576b749
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-09 16:05:53 +0800

    Update page.js

--- page.js
+++ page.js
@@ -3,7 +3,7 @@ class page{
     this.url = url;
     this.dir = dir || [];
     this.size = size || [];
-    this.tools = null;
+    this.tools = {};
     this.page = null;
     this.iframe = null;
   }
@@ -11,11 +11,12 @@ class page{
     let e = document.createElement("div");
     e.className = "adding-iframe-page center";
     p.appendChild(e);
+    this.bind();
     createIframe();
     createTools();
     this.page = e;
     let page = this.page;
-    return  this;
+    return this;
   }
   render(){
     let p = this.page;
@@ -24,16 +25,30 @@ class page{
     p.style.top = this.dir[1];
     p.style.width = this.size[0];
     p.style.height  = this.size[1];
-    return  this;
+    return this;
   }
   change({url,dir,size}){
     this.url = url || this.url;
     this.dir = dir || this.dir;
     this.size = size || this.size;
-    return  this;
+    render();
+    return this;
   }
   bind(){
-    
+    let p = this.page;
+    let _this = this;
+    p.addEventListener("touchstart",function(e){
+      let t = e.touches[0];
+      let beginDir = [t.pageX,t.pageY];
+      p.addEventListener("touchmove",function(e){
+        let t = e.touches[0];
+        let x = beginDir[0],
+            y = beginDir[1];
+        let addtion = [t.pageX - x,t.pageY - y];
+        let dir = [_this.dir[0] + addtion[0],_this.dir[1] + addtion[1]];
+        _this.change({dir});
+      });
+    });
   }
   createIframe(){
     let i = document.createElement("iframe");
@@ -41,33 +56,47 @@ class page{
     i.src = this.url;
     this.page.appendChild(i);
     this.iframe = i;
-    return  this;
+    return this;
   }
   createTools(p){
     let d = document.createElement("div");
     d.className = "pageTools";
     p.appendChild(d);
-    this.createCloseBtn(d);
-    this.createChangeBox(d);
+    this.tools.close = this.createCloseBtn(d);
+    this.tools.change = this.createChangeBox(d);
   }
   createCloseBtn(p){
     let b = document.createElement("div");
     b.className = "closePageButton";
     p.appendChild(b);
+    let page = this.page;
     b.addEventListener("click",function(e){
-      p.remove();
+      page.remove();
       b.remove();
     });
+    return b;
   }
   createChangeBox(p){
     let b = document.createElement("div");
     b.className = "pageChangeBox";
     b.innerHTML = `
       <input placeholder="url" class="pageURL">
-      <button class="pageURLSetBtn">修改地址</button>
       <input placeholder="请输入长度和宽度（用,相隔）" class="pageSize">
       <button class="pageChangeBtn">修改大小</button>`;
     p.appendChild(b)
-    
+    let page = this.page;
+    let url = b.querySelector(".pageURL"),
+        size = b.querySelector(".pageSize"),
+        btn = b.querySelector(".pageChangeBtn");
+    url.vaule = this.url;
+    size.value = this.size;
+    let _this = this;
+    b.addEventListener("click",function(){
+      _this.change({
+        url:url.value,
+        size:size.value
+      });
+    });
+    return b;
   }
 }
