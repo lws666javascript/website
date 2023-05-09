3503a9563c2e35aedbe29c8f882ee6838f3432a7
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-09 10:58:54 +0800

    Update page.js

--- page.js
+++ page.js
@@ -4,21 +4,31 @@ class page{
     this.dir = dir || [];
     this.size = size || [];
     this.tools = null;
+    this.page = null;
+    this.iframe = null;
   }
   show(p){
     let e = document.createElement("div");
     e.className = "adding-iframe-page center";
     p.appendChild(e);
-    this.ele.page = e;
+    createIframe();
+    createTools();
+    this.page = e;
+    let page = this.page;
+    return  this;
   }
-  render({url,dir,size}){
-    this.url = url;
-    this.dir = dir;
-    this.size = size;
-    
+  render(){
+    let p = this.page;
+    this.iframe.src = this.url;
+    p.style.left = this.dir[0];
+    p.style.top = this.dir[1];
+    p.style.width = this.size[0];
+    p.style.height  = this.size[1];
   }
-  move(){
-  
+  change({url,dir,size}){
+    this.url = url || this.url;
+    this.dir = dir || this.dir;
+    this.size = size || this.size;
   }
   bind(){
   
@@ -26,10 +36,13 @@ class page{
   createIframe(){
     let i = document.createElement("iframe");
     i.className = "adding-iframe";
-    this.ele.page.appendChild(i);
-    
+    i.src = this.url;
+    this.page.appendChild(i);
+    this.iframe = i;
+    return  this;
   }
   createTools(){
-  
+    
   }
+  
 }
