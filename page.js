d86a2c738b718c71ca9b804cd06cbf67120ccf86
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-10 13:53:11 +0800

    Update page.js

--- page.js
+++ page.js
@@ -1,5 +1,5 @@
 alert("go")
-/*try{class Page{
+try{class Page{
   constructor({url,dir,size}){
     this.url = url;
     this.dir = dir || [];
@@ -103,4 +103,4 @@ alert("go")
     return b;
   }
 console.log(Page)
-    }catch(e){console.log(e)}*/
+    }catch(e){alert(e)}
