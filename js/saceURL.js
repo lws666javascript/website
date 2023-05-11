724cfff27bcb23ea40648575aab1d77c0ab46176
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-11 16:10:07 +0800

    Update saceURL.js

--- js/saceURL.js
+++ js/saceURL.js
@@ -1,1 +1,12 @@
-
+let ls = localStorage;
+ls.savedURL = ls.savedURL || "[]";
+function saveURL(url){
+  let URL = JSON.parse(ls.savedURL);
+  URL.push(url);
+  ls.savedURL = JSON.stringify(URL);
+  return ls.savedURL;
+}
+function getURL(){
+  let URL = JSON.parse(ls.savedURL);
+  return URL;
+}
