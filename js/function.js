7fa92f85d749027429422664fa078266f87c8501
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-12 16:03:06 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,6 +1,44 @@
-function qryEle(text){
+function jq(text){
   let jq = document.querySelector(text);
-  return jq;
+  const o = {
+    self:jq,
+    size:[this.self.offsetWidth,this.self.offsetHeight],
+    dir:[this.self.offsetLeft,this.self.offsetTop],
+    style:(function(t){
+      let s = {};
+      for(let i in t.style){
+        s[i] = getComputedStyle(t,t[i]);
+      }
+      return s;
+    })(this.self),
+    bind(e,f){
+      this.self.addEventListener(e,f);
+      return this;
+    },
+    css(o){
+      let s = this.self;
+      for(let i in o){
+        s.style[i] = o[i];
+      }
+      return this;
+    },
+    size(s){
+      size = this.size = [s[0] || this.size[0],s[1] || this.size[1]];
+      this.css({
+        width:size[0],
+        height:size[1]
+      });
+      return this;
+    },
+    move(d){
+      dir = this.dir = [d[0] || this.dir[0],d[1] || this.dir[1]];
+      this.css({
+        left:dir[0],
+        right:dir[1]
+      });
+    }
+  }
+  return o;
 }
 const random = {
   random(){
