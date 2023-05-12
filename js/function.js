3496581a835efd061303a58aade75568b2bee51d
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-12 15:29:29 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -2,3 +2,20 @@ function qryEle(text){
   let jq = document.querySelector(text);
   return jq;
 }
+const random = {
+  randInt(x,y){
+    return Math.random() * (y - x) + x;
+  }
+  choice(arr){
+    return arr[this.randInt(0,arr.length - 1)];
+  }
+  sample(arr,k){
+    const a = [];
+    while(k){
+      let r = this.randInt(0,arr.length - 1);
+      if(arr[r] in a){
+        a.push(arr[r]);
+      }
+    }
+  }
+}
