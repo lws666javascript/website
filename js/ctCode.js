805a3f12a6a7a3b17efc0472e2e88ea73df50ef0
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-15 18:20:55 +0800

    Update ctCode.js

--- js/ctCode.js
+++ js/ctCode.js
@@ -6,9 +6,7 @@ function ctCode(str){
           p1 = s1.match(/^.*=/)[0].replace("=",""),
           p2 = s1.match(/=.*$/)[0].replace("=","");
       p2 = /,/.test(p2)?p2.split(","):p2;
-      return {
-        [p1]:p2
-      }
+      return {}[p1]=p2;
     }else{
       return reg.test(s);
     }
