7f9019ba80a7566a4f21271ab6dd0f620b0973b3
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-15 11:35:37 +0800

    Create ctCode.js

---
+++ js/ctCode.js
@@ -0,0 +1,15 @@
+function ctCode(str){
+  let reg = new RegExp(`\\\\${str}\\\\`);
+  reg.codeTest = function(s){
+    if(/^.*=.*$/.test(str)){
+      let s1 = s.match(reg);
+      let p1 = s1.match(/^.*=/).replace("=",""),
+          p2 = s1.match(/=.*$/).replace("=","");
+      return {
+        p1:p2
+      }                
+    }else{
+      return reg.test(s);
+    }
+  }
+}
