74275617e379f07639a0a283f15b5df74266415d
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-15 12:21:12 +0800

    Update colorText.js

--- js/colorText.js
+++ js/colorText.js
@@ -1,19 +1,19 @@
-function ctText(value,animation){
+function ctText(value){
     let p = jq.create("p");
     p.inner = value;
     p.class = "text"
     p.set();
-    p.animation(animation);
     jq.body.append(p);
+    return p;
 }
 jq("button").bind("click",function(){
     let i = jq("input"),
         IArr = i.self.value.split(","),
         t = ctCode("time=.*"),
-        ts = ctCode("textSize=.*"),
+        ts = ctCode("textSize=.*")(),
         text = IArr[0],
         code = IArr[1];
     ctText(text,function(){
         
-    });    
+    });
 });
