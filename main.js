2d50a9c1621c5168a85bc8d5b5a03d0dd6627dea
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-15 16:40:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ document.querySelector("#ConsoleCode").addEventListener("click",function(){
 });
 //绑定事件添加iframe
 jq("#UserSubmit").bind("click",function(){
-  let v = jq("#UserInput").self.value;
+  let v = jq("#UserInput").self.value,
+      data = JSON.parse(localStorage.Data);
   let body = document.body,
       w = body.offsetWidth,
       h = body.offsetHeight;
@@ -20,4 +21,6 @@ jq("#UserSubmit").bind("click",function(){
     dir:[w * 0.05,h * 0.05]
   });
   page.show(body);
+  data.times--;
+  localStorage.Data = JSON.stringify(data);
 });
