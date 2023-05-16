179c544ac7993e0702316387a98066950cf469f4
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-16 09:10:03 +0800

    Update ctCode.js

--- js/ctCode.js
+++ js/ctCode.js
@@ -2,12 +2,14 @@ function ctCode(str){
   let reg = new RegExp(`\\\\${str}\\\\`);
   reg.codeTest = function(s){
     if(/^.*=.*$/.test(str)){
+      if(s.match(reg)[0]){
       let s1 = s.match(reg)[0],
           p1 = s1.match(/.*=/)[0].replace(/\\|=/g,""),
           p2 = s1.match(/=.*$/)[0].replace(/\\|=/g,"");
       p2 = /,/.test(p2)?p2.split(","):p2;
       return {
         [p1]:p2
+      }
       }
     }else{
       return reg.test(s);
