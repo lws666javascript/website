165a6fa8da0b60c89b210c3f339e63dcef2d4588
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-16 14:39:53 +0800

    Update saveURL.js

--- js/saveURL.js
+++ js/saveURL.js
@@ -3,14 +3,14 @@ ls.savedURL = ls.savedURL || "[]";
 function saveURL(url){
   let URL = JSON.parse(ls.savedURL);
   if(/^https?/.test(url)){
-    if(!(url.replace(/\\.*\\/g,"") in URL)){
-      URL.push(url.replace(/\\.*\\/g,""));
+    if(!(url.replace(/(\\.*\\)| /g,"") in URL)){
+      URL.push(url.replace(/(\\.*\\)| /g,""));
       alert("保存成功！")
     }else if(setURL.nameURL(url)){
-      URL[URL.search(url.replace(/\\.*\\/g,""))] = url + " \\name=" + setURL.nameURL(url).name + "\\";
+      URL[URL.search(url.replace(/(\\.*\\)| /g,""))] = url + " \\name=" + setURL.nameURL(url).name + "\\";
       alert("命名成功！")
     }else if(setURL.delURL(url)){
-      URL.splice(URL.search(url.replace(/\\.*\\/g,"")),1);
+      URL.splice(URL.search(url.replace(/(\\.*\\)| /g,"")),1);
       alert("删除成功！")
     }else{
       alert("已经保存过了！");
