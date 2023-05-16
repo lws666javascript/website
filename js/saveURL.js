cfa779c5cb2b34954d60c86e7f66ecffc8f6188f
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-16 11:00:42 +0800

    Update saveURL.js

--- js/saveURL.js
+++ js/saveURL.js
@@ -4,9 +4,9 @@ function saveURL(url){
   let URL = JSON.parse(ls.savedURL);
   if(/^https?/.test(url)){
     URL.push(url);
-    alert("保存成功！")
+    alert("保存成功！");
   }else{
-    alert("请输入正确网址！")
+    alert("请输入正确网址！");
   }
   ls.savedURL = JSON.stringify(URL);
   return ls.savedURL;
