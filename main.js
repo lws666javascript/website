963f4fad35bd5b7391afa7324418377109670b76
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-16 20:44:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-navigator
+navigator.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/"
 //调试按钮
 document.querySelector("#ConsoleCode").addEventListener("click",function(){
   try{
