43978315b67814b13f4703c392993b83b6af7cee
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-19 09:51:44 +0800

    Update scaleElement.js

--- js/scaleElement.js
+++ js/scaleElement.js
@@ -1,3 +1,14 @@
-function setCtrlScaleElement(){
-  
+function setCtrlScaleElement(ele,inner){
+  let b = jq(ele),
+      d = jq.create("div",{class:"closed-scale-ele scale-element"});
+  d.inner = "<div class='scale-page-close-btn'>×</div>" + inner;
+  d.set();
+  b.bind('click',function(){
+    d.class = "showed-scale-ele scale-element";
+    d.set();
+  });
+  jq(d.self.firstChild()).bind("click",function(){
+    d.class = "closed-scale-ele scale-element";
+    d.set();
+  })
 }
