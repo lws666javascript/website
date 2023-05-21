4578842aeb4161aef6370d17c1da3516daf626ab
lws666javascript <119755836+lws666javascript@users.noreply.github.com>
2023-05-21 11:04:59 +0800

    Update changeUA.js

--- js/changeUA.js
+++ js/changeUA.js
@@ -1,1 +1,5 @@
-
+const UserAgent =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{version} Safari/537.36';
+Object.defineProperty(navigator,'userAgent',{
+  value:UserAgent,
+  writable:false
+});
